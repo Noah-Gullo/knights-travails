98e4a04b20bd3ffa2d503095203345609d3e3b6f
Noah Gullo <noahgulloingram@gmail.com>
2026-05-23 09:06:04 -0700

    feat: add checkPossibleMoves() and tests

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,10 +1,86 @@
-import {Coordinate, knightMoves} from "../src/index.js"
+import {Coordinate, checkPossibleMoves, knightMoves} from "../src/index.js"
 
-test("Invalid coordiantes", () => {
+test("Invalid coordiantes to knightMoves", () => {
     expect(() => {knightMoves(new Coordinate(-1, 3), new Coordinate(0, 5))}).toThrow();
     expect(() => {knightMoves(new Coordinate(0, 9), new Coordinate(0, 5))}).toThrow();
     expect(() => {knightMoves(new Coordinate(0, 5), new Coordinate(-1, 5))}).toThrow();
     expect(() => {knightMoves(new Coordinate(0, 5), new Coordinate(0, 100))}).toThrow();
+})
+
+test("Possible moves in middle of board", () => {
+    expect(checkPossibleMoves(new Coordinate(3,3))).toStrictEqual([
+        new Coordinate(4, 5),
+        new Coordinate(5, 4),
+        new Coordinate(5, 2),
+        new Coordinate(4, 1),
+        new Coordinate(2, 1),
+        new Coordinate(1, 2),
+        new Coordinate(1, 4),
+        new Coordinate(2, 5),
+    ]);
+
+    expect(checkPossibleMoves(new Coordinate(4, 5))).toStrictEqual([
+        new Coordinate(5,7),
+        new Coordinate(6,6),
+        new Coordinate(6,4),
+        new Coordinate(5,3),
+        new Coordinate(3,3),
+        new Coordinate(2,4),
+        new Coordinate(2,6),
+        new Coordinate(3,7),
+    ]);
+})
+
+test("Possible moves on the edge, not corner, of the board", () => {
+    expect(checkPossibleMoves(new Coordinate(7, 3))).toStrictEqual([
+        new Coordinate(6,1),
+        new Coordinate(5,2),
+        new Coordinate(5,4),
+        new Coordinate(6,5),
+    ]);
+
+    expect(checkPossibleMoves(new Coordinate(3, 0))).toStrictEqual([
+        new Coordinate(4,2),
+        new Coordinate(5,1),
+        new Coordinate(1,1),
+        new Coordinate(2,2),
+    ]);
+
+    expect(checkPossibleMoves(new Coordinate(0, 3))).toStrictEqual([
+        new Coordinate(1,5),
+        new Coordinate(2,4),
+        new Coordinate(2,2),
+        new Coordinate(1,1),
+    ]);
+
+    expect(checkPossibleMoves(new Coordinate(3, 7))).toStrictEqual([
+        new Coordinate(5,6),
+        new Coordinate(4,5),
+        new Coordinate(2,5),
+        new Coordinate(1,6),
+    ])
+})  
+
+test("Possible moves for each corner", () => {
+    expect(checkPossibleMoves(new Coordinate(0,0))).toStrictEqual([
+        new Coordinate(1,2),
+        new Coordinate(2,1),
+    ])
+
+    expect(checkPossibleMoves(new Coordinate(7,0))).toStrictEqual([
+        new Coordinate(5,1),
+        new Coordinate(6,2),
+    ])
+
+    expect(checkPossibleMoves(new Coordinate(0,7))).toStrictEqual([
+        new Coordinate(2,6),
+        new Coordinate(1,5),
+    ])
+
+    expect(checkPossibleMoves(new Coordinate(7,7))).toStrictEqual([
+        new Coordinate(6,5),
+        new Coordinate(5,6),
+    ])
 })
 
 test("Shortest path for more than 1 jump", () => {
--- src/index.js
+++ src/index.js
@@ -5,13 +5,44 @@ export class Coordinate{
     }
 }
 
+function validCoordinate(point){
+    if(!(point instanceof Coordinate)) throw Error("validCoordinate requires a Coordinate argument.");
+    if(point.x < 0 || point.x > 7 || point.y < 0 || point.y > 7){
+        return false;
+    }
+    return true;
+}
+
+export function checkPossibleMoves(point){
+    const offsets = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]];
+    let validMoves = [];
+
+    for(let i = 0; i < offsets.length; i++){
+        const xOffset = offsets[i][0];
+        const yOffset = offsets[i][1];
+        const currMove = new Coordinate(point.x + xOffset, point.y + yOffset);
+
+        if(validCoordinate(currMove)){
+            validMoves.push(currMove);
+        }
+    }
+
+    return validMoves;
+}
+
 export function knightMoves(start, end){
     if(!(start instanceof Coordinate) || !(end instanceof Coordinate)) throw Error("knightMoves argument must be two Coordinates.");
-    if(start.x < 0 || start.x > 7 || start.y < 0 || start.y > 7) throw Error("Starting coordinate is out of bounds.");
-    if(end.x < 0 || end.x > 7 || end.y < 0 || end.y > 7) throw Error("Starting coordinate is out of bounds.");
+    if(!validCoordinate(start)) throw Error("Starting coordinate is out of bounds.");
+    if(!validCoordinate(end)) throw Error("Starting coordinate is out of bounds.");
 
     let visitQueue = []; 
     let visited = [];
 
     visitQueue.push(start);
+
+    while(visitQueue.length != 0){
+        checkPossibleMoves(visit[0]);
+        visited.push(visitQueue[0]);
+        visitQueue.splice(0, 1);
+    }
 }
